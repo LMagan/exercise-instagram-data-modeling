4274fb692c9855043a97e543b7ee9949a682740f
L.Magán <lucasfliper@hotmail.com>
2025-01-30 17:15:53 +0000

    first-commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(50), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+
+class Comment(Base):
+    __tablename__ ='comment'
+
+    comment_id = Column(Integer, primary_key=True)
+    comment_text = Column(String(50), nullable=True)
+    author_id = Column(Integer, ForeignKey('user.user_id'))
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    media_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    type = Column(String(20), nullable=False)
+    url = Column(String(20), nullable=True)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    follower_id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.user_id'))
+    user_to_id = Column(Integer, ForeignKey('user.user_id'))
 
     def to_dict(self):
         return {}
